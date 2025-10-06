613a1a88b93f1fc5c4dadd347832a4dc108c8cc1
Leonardo Sima <leosima.lc@gmail.com>
2025-10-06 15:22:39 -0300

    :sparkles: feat: cria comando para sortear filme

---
+++ commands/sortear-filme.js
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder } from "discord.js";
+import { query } from "../data/db.js";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("sortear")
+        .setDescription("Sorteia um filme aleatoriamente"),
+    async execute(interaction) {
+        try {
+            const sortearFilmeQuery = `
+                SELECT nome_filme FROM filmes_sugeridos ORDER BY RANDOM() LIMIT 1;
+            `;
+            const { rows } = await query(sortearFilmeQuery);
+
+            if (rows.length === 0) {
+                return interaction.reply("Erro ao sortear filme: nenhum filme encontrado");
+            }
+
+            const embed = new EmbedBuilder()
+                .setTitle("Filme Sorteado")
+                .setDescription(rows[0].nome_filme)
+                .setColor(0x00AE86);
+
+            await interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            console.error("Erro ao sortear filme:", error);
+            await interaction.reply("Ocorreu um erro ao sortear um filme");
+        }
+    }
+};
